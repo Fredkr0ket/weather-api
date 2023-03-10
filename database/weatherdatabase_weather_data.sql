6d8a47d952e7968f0f002d22a991c85877fb0788
Fredkroket <ferdovbalen@gmail.com>
2023-03-10 10:44:33 +0100

    added new database dump

--- database/weatherdatabase_weather_data.sql
+++ database/weatherdatabase_weather_data.sql
@@ -1,9 +1,13 @@
--- Server version	8.0.32
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: weatherdatabase
+-- ------------------------------------------------------
+-- Server version	8.0.32-0ubuntu0.22.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -19,13 +23,13 @@ DROP TABLE IF EXISTS `weather_data`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `weather_data` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `location` varchar(200) DEFAULT NULL,
+  `weather_id` int NOT NULL AUTO_INCREMENT,
+  `weather_location` varchar(100) DEFAULT NULL,
   `temperature` int NOT NULL,
   `humidity` int NOT NULL,
   `weather_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`weather_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -34,7 +38,7 @@ CREATE TABLE `weather_data` (
 
 LOCK TABLES `weather_data` WRITE;
 /*!40000 ALTER TABLE `weather_data` DISABLE KEYS */;
-INSERT INTO `weather_data` VALUES (1,'drachten',20,20,'2023-02-20 15:58:07'),(2,'idstein',40,50,'2023-02-20 15:58:45');
+INSERT INTO `weather_data` VALUES (1,'drachten',123,12,'2023-03-06 11:52:23'),(2,'baas',20,20,'2023-03-07 10:52:19'),(3,'drachten',20,20,'2023-03-07 10:52:35');
 /*!40000 ALTER TABLE `weather_data` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -47,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-24 17:38:54
+-- Dump completed on 2023-03-10 10:42:36
